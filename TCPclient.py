e5dde3ff5f7b973f7e6140dd86b756fa3b513a2a
ei <chronolavos0124@gmail.com>
2018-09-18 10:21:24 -0400

    Add TCPserver and UDPserver

--- TCPclient.py
+++ TCPclient.py
@@ -2,14 +2,19 @@
 
 import socket
 
-target_host = "www.google.com"
-target_port = 80
+#target_host = "www.google.com"
+#target_port = 80
+
+target_host = "0.0.0.0"
+target_port = 80 
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect((target_host, target_port))
 
-client.send("Get / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+#client.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+
+client.send(b"GET / HTTP/1.1\r\nHost: 0.0.0.0\r\n\r\n")
 
 response = client.recv(4096)
 
